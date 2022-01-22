e09f528d5baab3fc241ec667eb070ed1ae10c6b5
Federico Lopez <fedelop_10@hotmail.com>
2022-01-22 20:18:15 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+const NavBar = () =>{
+    return (
+    <nav className='navBar'>
+    <ul className='listado'>
+      <li className='item'>Home</li>
+      <li className='item'>Productos</li>
+      <li className='item'>Contacto</li>
+    </ul>
+  </nav>
+    );
+    };
+
+export default NavBar;
+
